f804a1f1e490e1c7a0822f54a851a0c014f6d9d0
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-09-19 22:46:21 +0300

    set parameter for creating password in create_user function.

--- fruits/management/commands/create_user.py
+++ fruits/management/commands/create_user.py
@@ -10,8 +10,9 @@ class Command(BaseCommand):
         # Create user
         params = dict(username='test', email='test@example.com', password='qwerty')
         user, user_status = User.objects.get_or_create(**params)
-        # if user_status:
-        #     user.is_staff = True
-        #     user.is_superuser = True
-        #     user.save()
+        if user_status:
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(params['password'])
+            user.save()
         print([f'User has already exists.', 'User was created successfully.'][user_status])
